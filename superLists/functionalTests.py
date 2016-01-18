691c0b5fc99b95ab2e02c47e707c28208d9fed25
m516 <m516@m516>
2016-01-18 11:29:41 -0300

    第一個功能測試包含註解並開始使用單元測試.

--- superLists/functionalTests.py
+++ superLists/functionalTests.py
@@ -1,6 +1,36 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        profile = webdriver.FirefoxProfile()
+        profile.set_preference('browser.startup.homepage', 'about:blank')
+        profile.set_preference('startup.homepage_welcome_url', 'about:blank')
+        profile.set_preference('startup.homepage_welcome_url.additional', 'about:blank')
+        self.browser = webdriver.Firefox(profile)
+        self.browser.implicitly_wait(3) 
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+        # 彤彤聽說有一款很酷的待辦事項應用程式，她前往它的首頁
+        self.browser.get('http://localhost:8000')
+
+        # 她注意到首頁的標題提到了待辦事項清單
+        # assert '待辦事項' in browser.title
+        self.assertIn('待辦事項', self.browser.title)
+        self.fail('Finish the test！') 
+
+        # 網站邀請她輸入一個待辦事項
+
+        ...
+
+        # 她很滿意，就上床睡覺了
+
+
+if __name__=='__main__':
+    unittest.main(warnings='ignore')
