6277ec8408d2f3be4c17acce5dd70348e6b68586
m516 <m516@m516>
2016-01-19 11:01:05 -0300

    建立清單項目Model與資料庫Migration

--- superLists/lists/tests.py
+++ superLists/lists/tests.py
@@ -3,6 +3,7 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.shortcuts import render
 from lists.views import homePage
+from lists.models import Item
 
 
 # Create your tests here.
@@ -24,3 +25,22 @@ class HomePageTest(TestCase):
         if expectedHTML:
             expectedHTML = expectedHTML.content.decode('UTF-8')
         self.assertEqual(response, expectedHTML)
+
+class ItemModelTest(TestCase):
+    
+    def test_saving_and_retrieving_items(self):
+        firstItem = Item()
+        firstItem.text = '第一個清單項目'
+        firstItem.save()
+        
+        secondItem = Item()
+        secondItem.text = '第二個清單項目'
+        secondItem.save()
+        
+        savedItems = Item.objects.all()
+        self.assertEqual(savedItems.count(), 2)
+        
+        firstSavedItem = savedItems[0]
+        secondSavedItem = savedItems[1]
+        self.assertEqual(firstSavedItem.text, '第一個清單項目')
+        self.assertEqual(secondSavedItem.text, '第二個清單項目')
