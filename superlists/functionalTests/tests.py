a3c6a66270bb9fc16cf12b1000fac9b1a607e9cc
m516 <m516@m516>
2016-01-21 12:37:46 -0300

    建立base.html並使用範本繼承

--- superlists/functionalTests/tests.py
+++ superlists/functionalTests/tests.py
@@ -82,3 +82,16 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('買鮮奶', pageText)
         
         # 兩個人都很滿意，就都上床睡覺了
+        
+    def test_layout_and_styling(self):
+        # 彤彤前往首頁
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        
+        # 她注意到輸入框是置中對齊的
+        inputbox = self.browser.find_element_by_id('newItem')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=5
+        )
