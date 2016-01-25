d5c0a90da9c1a9de3d1874fffc3fdec523edf02b
m516 <m516@m516>
2016-01-25 11:36:56 -0300

    新的FT：清單項目不能空白，測試失敗

--- superlists/functionalTests/test_list_item_validation.py
+++ superlists/functionalTests/test_list_item_validation.py
@@ -2,19 +2,30 @@ from unittest import skip
 from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
-    
-    
-    @skip 
+
+
     def test_cannot_add_empty_list_items(self):
         # 彤彤前往首頁，一不小心按下ENTER鍵，送出了一個空的項目
-        
-        # 頁面更新，出現一個錯誤訊息：項目不能空白
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('newItem').send_keys('\n')
+         
+        # 頁面更新，出現一個錯誤訊息：清單項目不能空白
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, '清單項目不能空白')
         
         # 她再試一次，輸入文字，這次運作良好
-        
+        self.browser.find_element_by_id('newItem').send_keys('買鮮奶\n')
+        self.check_for_row_in_listTable('買鮮奶')
+                
         # 她頑皮地又輸入了一次空白項目
-        
+        self.browser.find_element_by_id('newItem').send_keys('\n')
+
         # 她還是看到了同樣的錯誤訊息
-        
+        self.check_for_row_in_listTable('買鮮奶')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, '清單項目不能空白')
+
         # 她可以輸入文字來更正錯誤
-        self.fail('填資料吧!')
+        self.browser.find_element_by_id('newItem').send_keys('泡茶\n')
+        self.check_for_row_in_listTable('買鮮奶')
+        self.check_for_row_in_listTable('泡茶')
